9015128683bae4a63aad92bcfa39cbd4877cecfb
Sabina Buzatu <sabina.andreea.buzatu@gmail.com>
2024-11-15 23:28:53 +0200

    Spring project initialization

---
+++ src/test/java/com/store/storemanagement/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
